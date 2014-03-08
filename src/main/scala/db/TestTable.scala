8fae95d6582ac3bb7d4f98d124d49c165b948cb8
Christian Papauschek <christian@papauschek.at>
2014-03-08 12:03:59 +0100

    Slick sample code

---
+++ src/main/scala/db/TestTable.scala
@@ -0,0 +1,22 @@
+package db
+
+import slick.driver.JdbcDriver.simple._
+
+case class Row(id: Int)
+
+class Rows(tag: Tag) extends Table[Row](tag, "table") {
+  def id = column[Int]("id")
+  def * = (id) <> (Row, Row.unapply _)
+}
+
+object Rows extends TableQuery(new Rows(_)) {
+
+  def test : Query[Column[Int], Int] = {
+    (for {
+      r <- Rows
+      id = r.id // commenting this out fixes the problem
+    } yield(r.id))
+  }
+
+}
+
---
+++ src/test/scala/db/SlickTests.scala
@@ -0,0 +1,13 @@
+package db
+
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class SlickTests extends FunSuite {
+
+  test("Dummy test") {
+    assert(true === true)
+  }
+}
